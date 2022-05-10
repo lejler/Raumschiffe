312413b6400db4e72c7783b75bc464a135051d14
llayla <layla.shokri@gmx.de>
2022-05-10 23:26:39 +0200

    quicksave

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        Ladung ladung = new Ladung(5, 6, 7, 0, 1);
+        Raumschiff r = new Raumschiff("Hans", "kapitaen", 100, 100, 100, 100, 4, 5, ladung);
+        r.showZustand();
     }
 }
--- src/Raumschiff.java
+++ src/Raumschiff.java
@@ -1,21 +1,26 @@
 import java.util.ArrayList;
 
 public class Raumschiff {
+
+    private String name;
+    private String kapitaen;
     private int energieversorgungInProzent;
     private int schutzschildeInProzent;
     private int lebenserhaltungssystemeInProzent;
     private int huelleInProzent;
     private int anzahlTorpedos;
     private int anzahlAndroiden;
-    private static final ArrayList<String> BROADCAST_COMMUNICATOR;
+    private static final ArrayList<String> BROADCAST_COMMUNICATOR = new ArrayList<>();
     private Ladung ladungsverzeichnis;
     
     public Raumschiff() {
     }
 
-    public Raumschiff(int energieversorgungInProzent, int schutzschildeInProzent, int lebenserhaltungssystemeInProzent, 
+    public Raumschiff(String name, String kapitaen, int energieversorgungInProzent, int schutzschildeInProzent, int lebenserhaltungssystemeInProzent, 
                       int huelleInProzent, int anzahlTorpedos, int anzahlAndroiden, Ladung ladungsverzeichnis) {
-                          
+                 
+        this.name = name;
+        this.kapitaen = kapitaen;
         this.energieversorgungInProzent = energieversorgungInProzent;
         this.schutzschildeInProzent = schutzschildeInProzent;
         this.lebenserhaltungssystemeInProzent = lebenserhaltungssystemeInProzent;
@@ -25,20 +30,60 @@ public class Raumschiff {
         this.ladungsverzeichnis = ladungsverzeichnis;
     }
 
+    public void showZustand() {
+
+        System.out.println("Name: " + name);
+        System.out.println("Kapitän: " + kapitaen);
+        System.out.println("Energieversorgung: " + energieversorgungInProzent + "%");
+        System.out.println("Schutzschilde: " + schutzschildeInProzent + "%");
+        System.out.println("Lebenserhaltungssysteme: " + lebenserhaltungssystemeInProzent + "%");
+        System.out.println("Hülle: " + huelleInProzent + "%");
+        System.out.println("Torpedos: " + anzahlTorpedos);
+        System.out.println("Androiden: " + anzahlAndroiden);
+        System.out.print("Ladung: ");
+        System.out.print("Tricoder: " + ladungsverzeichnis.getAnzahlTricoder() + ", ");
+        System.out.print("Forschungssonden: " + ladungsverzeichnis.getAnzahlForschungssonden() + ", ");
+        System.out.print("Klingonen: " + ladungsverzeichnis.getAnzahlKlingonen() + ", ");
+        System.out.print("Romulaner: " + ladungsverzeichnis.getAnzahlRomulaner() + ", ");
+        System.out.print("Valkanier: " + ladungsverzeichnis.getAnzahlVulkanier());
+    }
+
     public void shootTorpedo(Raumschiff raumschiff) {
-        //TODO
+        if (anzahlTorpedos == 0) {
+            System.out.println("-=*Click*=-");
+            return;
+        }
+        anzahlTorpedos-=1;
+        notifyAll("Photonentorpedo abgeschossen");
+        hit();
     }
 
     public void shootKanone(Raumschiff raumschiff) {
-        //TODO
+        if (energieversorgungInProzent < 50) {
+            System.out.println("-=*Click*=-");
+            return;
+        }
+        energieversorgungInProzent-= 50;
+        notifyAll("Phaserkanone abgeschossen");
+        hit();
     }
 
     private void hit() {
-        //TODO
+        schutzschildeInProzent -= 50;
+        if (schutzschildeInProzent < 0) {
+            huelleInProzent -= 50;
+            energieversorgungInProzent -= 50;
+            // ich weiß, dass die Aufgabe vorgibt, bei 0% diese Nachricht ausgeben zu lassen, finde es so aber sinnvoller
+            if (huelleInProzent < 0) {
+                notifyAll("Lebenserhaltungssysteme wurden zerstört!");
+            }
+        }
+        System.out.println(name + " wurde getroffen!");
     }
 
     public void notifyAll(String message) {
         System.out.println(message);
+        BROADCAST_COMMUNICATOR.add(message);
     }
 
     public void addLadung(Ladung ladung) {
@@ -46,7 +91,7 @@ public class Raumschiff {
     }
 
     public ArrayList<String> showLogbuchEintraege(ArrayList<String> broadcastCommunicator) {
-        return null;
+        return BROADCAST_COMMUNICATOR;
     }
 
     public void rohreLaden(int anzahlTorpedos) {
@@ -57,8 +102,28 @@ public class Raumschiff {
         
     }
 
+    public void addLogEntry(String logEntry) {
+
+    }
+
     private boolean hasTorpedos() {
         return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKapitaen() {
+        return kapitaen;
+    }
+
+    public void setKapitaen(String kapitaen) {
+        this.kapitaen = kapitaen;
     }
 
     public int getEnergieversorgungInProzent() {
@@ -109,10 +174,6 @@ public class Raumschiff {
         this.anzahlAndroiden = anzahlAndroiden;
     }
 
-    public static ArrayList<String> getBroadcastCommunicator() {
-        return BROADCAST_COMMUNICATOR;
-    }
-
     public Ladung getLadungsverzeichnis() {
         return ladungsverzeichnis;
     }
@@ -120,7 +181,5 @@ public class Raumschiff {
     public void setLadungsverzeichnis(Ladung ladungsverzeichnis) {
         this.ladungsverzeichnis = ladungsverzeichnis;
     }
-
-    
 
 }
