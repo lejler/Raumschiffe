e0ddfe30f0ee739ccb0e5a0aa82818c37940abef
llayla <layla.shokri@gmx.de>
2022-05-12 20:07:16 +0200

    quicksave

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Ladung l = new Ladung("Stuff", 2);
         Ladung l2 = new Ladung("Kram", 7);
         Ladung l3 = new Ladung("Kanonen", 5);
-        Ladung[] ladung = new Ladung[]{l, l2, l3};
-        Raumschiff r = new Raumschiff("Name", "kapitaen", 100, 100, 2, 100, 100, ladung);
+        ArrayList<Ladung> ladung = new ArrayList<>();
+        ladung.add(l);
+        ladung.add(l2);
+        ladung.add(l3);
+        //Ladung[] ladung = new Ladung[]{l, l2, l3};
+        Raumschiff r = new Raumschiff("Name", "kapitaen", 100, 100, 2, 4, 100, 100, ladung);
         r.showZustand();
     }
 }
